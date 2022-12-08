68d4f58861cab023e7b497a720753a7b331d6a0a
asmwarrior <asmwarrior@gmail.com>
2022-12-08 16:45:43 +0800

    * disable the code for drawing on the DC, use wxGraphicsContext to draw directly on wxBitmap to get the font with transparent background

--- svg_panel.cpp
+++ svg_panel.cpp
@@ -89,9 +89,13 @@ void SVGPanel::OnPaint(wxPaintEvent& event)
 			render_height = GetClientSize().y;
 		}
 
-		if( !m_bitmap.IsOk() || render_width != m_bitmap.GetWidth() || render_height != m_bitmap.GetHeight() )
+		if(render_width == 0 || render_height == 0)
+			return;
+
+		if(!m_bitmap.IsOk() || render_width != m_bitmap.GetWidth() || render_height != m_bitmap.GetHeight())
 		{
 			m_bitmap.Create(render_width, render_height, 32);
+			m_bitmap.UseAlpha(true);
 
 			NSVGrasterizer* rast = nsvgCreateRasterizer();
 			unsigned char* image_buffer = (unsigned char*)malloc(render_width * render_height * 4);
@@ -123,6 +127,56 @@ void SVGPanel::OnPaint(wxPaintEvent& event)
 			}
 			nsvgDeleteRasterizer(rast);
 			free(image_buffer);
+			// draw text on top
+			wxGraphicsContext* gc = wxGraphicsContext::Create(m_bitmap);
+			//gc->SetAntialiasMode( wxANTIALIAS_NONE );
+			for(TextLabel& a : m_TextLabels)
+			{
+				// The coordinates refer to the top-left corner of the rectangle bounding the string.
+				// See GetTextExtent() for how to get the dimensions of a text string,
+				// which can be used to position the text more precisely
+
+				// for svg text, its x,y is defined as: x: The x coordinate of the starting point of the text baseline.
+				// so generally the left bottom corner of the first character
+
+				//wxFont font(a.fontSize*4*g_Scale, wxROMAN, wxNORMAL, wxLIGHT, false, _T("Times New Roman"));
+
+				// 2022-04-10 point size is in pt unit, we have to convert it to px unit
+				// One point(pt) is the equivalent of 1.333(3) pixels(px)
+
+				wxFont font(wxFontInfo(a.fontSize * m_Scale / 1.3333).FaceName(a.family));
+
+				// enable the smooth font by default
+				// font rendering issue when using C::B under windows remote desktop
+				// https://forums.codeblocks.org/index.php/topic,25146.msg171484.html#msg171484
+
+				wxString nativeDesc = font.GetNativeFontInfoDesc();
+				int index = 0;
+				for(size_t pos = 0, start = 0; pos <= nativeDesc.length();)
+				{
+					pos = nativeDesc.find(";", start);
+					index++;
+					if(index == 14)
+					{
+						// enable the cleartype option of the font
+						nativeDesc.replace(start, pos - start, "5");
+						bool result = font.SetNativeFontInfo(nativeDesc);
+						break;
+					}
+					start = pos + 1;
+				}
+
+				wxGraphicsFont gfont = gc->CreateFont(font, *wxRED);
+				gc->SetFont(gfont);
+
+				// size calculation
+				wxDouble  w, h;
+				wxDouble descent;
+				gc->GetTextExtent(a.label, &w, &h, &descent);
+
+				gc->DrawText(a.label, a.x * m_Scale, a.y * m_Scale - h + descent);
+			} //for (TextLabel &a : m_TextLabels)
+			delete gc;
 		}
 
 		if(m_bitmap.IsOk())
@@ -131,7 +185,7 @@ void SVGPanel::OnPaint(wxPaintEvent& event)
 			//m_bitmap.SaveFile( "d:\\_nano_svg_test.png", wxBITMAP_TYPE_PNG );
 		}
 
-        // show text labels on top of the bitmap
+#if 0 // disable drawing the text labels on DC, we just need to build it on bitmap
 		// show text labels on top of the bitmap
 		for(TextLabel& a : m_TextLabels)
 		{
@@ -157,5 +211,6 @@ void SVGPanel::OnPaint(wxPaintEvent& event)
 
 			dc.DrawText(a.label, a.x * m_Scale, a.y * m_Scale - h + descent);
 		} //for (TextLabel &a : m_TextLabels)
+#endif
 	} //if( m_svg_image!=NULL )
 };
