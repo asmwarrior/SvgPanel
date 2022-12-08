910f578e284bd1dfbffc595ccc12f8d303d3d231
asmwarrior <asmwarrior@gmail.com>
2022-12-08 16:38:53 +0800

    - code format, no functional changes

--- svg_panel.h
+++ svg_panel.h
@@ -6,30 +6,30 @@
 // for showing the text label on the SVGPanel
 struct TextLabel
 {
-    wxString label;     /// the text content of the label
-    float x;            /// x coordinates of label
-    float y;            /// y coordinates of label
-    wxString family;    /// font name string, e.g. Arial, Courier New
-    float fontSize;     /// font size
+	wxString label;     /// the text content of the label
+	float x;            /// x coordinates of label
+	float y;            /// y coordinates of label
+	wxString family;    /// font name string, e.g. Arial, Courier New
+	float fontSize;     /// font size
 };
 
 struct NSVGimage;
 class SVGPanel : public wxPanel
 {
 public:
-	SVGPanel(wxWindow *parent, int id, const wxString &svg_filename = wxEmptyString);
+	SVGPanel(wxWindow* parent, int id, const wxString& svg_filename = wxEmptyString);
 	~SVGPanel();
-	void OnPaint(wxPaintEvent &event);
+	void OnPaint(wxPaintEvent& event);
 
-	void LoadSVG( const wxString &filename );
+	void LoadSVG(const wxString& filename);
 
 private:
-	NSVGimage *m_svg_image;
+	NSVGimage* m_svg_image;
 	wxBitmap m_bitmap;
 
 	// for showing the text label on the SVGPanel
 	std::vector<TextLabel> m_TextLabels;
-    float m_Scale; /// user scaled from m_svg_image to m_bitmap
+	float m_Scale; /// user scaled from m_svg_image to m_bitmap
 
 	DECLARE_EVENT_TABLE()
 };
